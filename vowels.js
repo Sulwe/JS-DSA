ddae6230a63b2fa40512a705b2ad979237d891c1
Sulwe <cypriansulwe.cs@gmail.com>
2024-11-21 13:32:17 +0300

    test for vowels by checking characters against an array of vowels

--- vowels.js
+++ vowels.js
@@ -1,6 +1,12 @@
 function vowels(str) {
-    const matches  = str.match(/[aeiou]/gi);
-    return matches ? matches.length : 0;
+    const vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+
+    for(let char of str.toLowerCase()) {
+        if (vowelCheck.includes(char)) count ++;
+    }
+    return count;
+
 }
 
 console.log(vowels('my name is me'));
