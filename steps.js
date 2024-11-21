ad40c97b8a4ff7a2352229fcea435ed3ea0ee470
Sulwe <cypriansulwe.cs@gmail.com>
2024-11-21 15:14:09 +0300

    create steps using nested loop

--- steps.js
+++ steps.js
@@ -1,6 +1,16 @@
 function steps(n) {
-    for (let i = 1; i <= n; i ++) {
-        console.log('#'.repeat(i));
+    for (let row = 1; row <= n; row ++) {
+        let line = '';
+        for (let col = 1; col <= n; col ++) {
+            if (col <= row) {
+                line += '#';
+            }
+            else {
+                line += ' ';
+            }
+            
+        }
+        console.log(line);
     }
 }
 
