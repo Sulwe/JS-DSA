0c0460787888285ea8983eb51f85f79f64f5a42a
Sulwe <cypriansulwe.cs@gmail.com>
2024-11-25 15:16:30 +0300

    sort active users by age in ascending order

--- users.js
+++ users.js
@@ -3,19 +3,23 @@ const users = [
         id: 1,
         name: 'Jack',
         isActive: true,
+        age: 40,
     },
     {
         id: 2,
         name: 'John',
         isActive: true,
+        age: 23,
     },
     {
         id: 3,
         name: 'Mike',
         isActive: false,
+        age: 56,
     },
 ];
-let names = users.filter((user) => user.isActive).map((user) => user.name)
+users.sort((user1, user2) => user1.age < user2.age ? -1 : 1)
+const names = users.filter((user) => user.isActive).map((user) => user.name);
 
 console.log(names);
 
