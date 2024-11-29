de1ed5be778d9a96a42de8d0267d649370572dd3
Sulwe <cypriansulwe.cs@gmail.com>
2024-11-29 14:15:04 +0300

    calculate the nth fibonacci value

--- recursion.js
+++ recursion.js
@@ -1,4 +1,4 @@
-function pow(x, n) {
+/*function pow(x, n) {
    if (n === 0){
     return 1;
    }
@@ -8,3 +8,22 @@ function pow(x, n) {
 }
 
 console.log(pow(10, 0));
+*/
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    }
+    else if (n === 1) {
+        return 1;
+    }
+    else if (n === 2) {
+        return 1;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
+       
+}
+console.log(fib(8))
+
